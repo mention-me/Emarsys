cc2360999dd002f1de01a6b87094a5fea648bad1
Alex Oanea <alex.oanea@mention-me.com>
2021-03-22 20:05:37 +0100

    formatting

--- tests/Unit/Suites/ResponseTest.php
+++ tests/Unit/Suites/ResponseTest.php
@@ -14,61 +14,61 @@ class ResponseTest extends TestCase
     /**
      * @throws ClientException
      */
-	public function testItThrowsClientException(): void
+    public function testItThrowsClientException(): void
     {
         $this->expectException(ClientException::class);
         $this->expectErrorMessage('Unexpect response structure, no replyCode or replyText');
-		$dummyResult = array('dummy');
-		new Response($dummyResult);
-	}
+        $dummyResult = ['dummy'];
+        new Response($dummyResult);
+    }
 
     /**
      * @throws ClientException
      */
-	public function testItGetsResponseData(): void
+    public function testItGetsResponseData(): void
     {
-		$expectedResponse = $this->createExpectedResponse('createContact');
-		$result = new Response($expectedResponse);
+        $expectedResponse = $this->createExpectedResponse('createContact');
+        $result = new Response($expectedResponse);
 
-		self::assertNotEmpty($result);
-
-	}
+        self::assertNotEmpty($result);
+    }
 
     /**
      * @throws ClientException
      */
-	public function testItSetsAndGetsReplyCode(): void
+    public function testItSetsAndGetsReplyCode(): void
     {
-		$expectedResponse = $this->createExpectedResponse('createContact');
-		$result = new Response($expectedResponse);
+        $expectedResponse = $this->createExpectedResponse('createContact');
+        $result = new Response($expectedResponse);
 
-		self::assertSame(Response::REPLY_CODE_OK, $result->getReplyCode());
-	}
+        self::assertSame(Response::REPLY_CODE_OK, $result->getReplyCode());
+    }
 
     /**
      * @throws ClientException
      */
-	public function testItSetsAndGetsReplyText(): void
+    public function testItSetsAndGetsReplyText(): void
     {
-		$expectedResponse = $this->createExpectedResponse('createContact');
-		$result = new Response($expectedResponse);
+        $expectedResponse = $this->createExpectedResponse('createContact');
+        $result = new Response($expectedResponse);
 
-		self::assertEquals('OK', $result->getReplyText());
-	}
+        self::assertEquals('OK', $result->getReplyText());
+    }
 
     /**
      * @throws ClientException
      */
-	public function testItResponseWithoutData(): void
+    public function testItResponseWithoutData(): void
     {
-		$expectedResponse = $this->createExpectedResponse('insertRecord');
-		$result = new Response($expectedResponse);
+        $expectedResponse = $this->createExpectedResponse('insertRecord');
+        $result = new Response($expectedResponse);
 
-		self::assertEmpty($result->getData());
-	}
+        self::assertEmpty($result->getData());
+    }
 
-	/**
+    /**
      * @param string $fileName
+     *
      * @return mixed
      */
     private function createExpectedResponse(string $fileName)
